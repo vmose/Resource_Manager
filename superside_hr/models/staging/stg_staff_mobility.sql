d2a08d729675b931acb06e96d8d56154c7d92afe
vmose <vicmose.vm@gmail.com>
2024-09-26 10:59:58 +0300

    fix_errors

--- superside_hr/models/staging/stg_staff_mobility.sql
+++ superside_hr/models/staging/stg_staff_mobility.sql
@@ -1,11 +1,11 @@
-WITH cleaned_staff_mobility AS (
-    SELECT 
+with cleaned_staff_mobility as (
+    select 
         Name,
-        `Date of Mobility` AS mobility_date,
-        `Previous Role` AS previous_role,
-        `Previous Manager` AS previous_manager,
-        `Previous Job Level` AS previous_job_level,
-        `Previous Functional Group` AS previous_functional_group
-    FROM `glass-mantra-128509.superside_hr.staff_mobility`
+        `Date of Mobility` as mobility_date,
+        `Previous Role` as previous_role,
+        `Previous Manager` as previous_manager,
+        `Previous Job Level` as previous_job_level,
+        `Previous Functional Group` as previous_functional_group
+    from `glass-mantra-128509.superside_hr.staff_mobility`
 )
-SELECT * FROM cleaned_staff_mobility;
+select * from cleaned_staff_mobility;
