6d1cbf9cf526c3a47c35ec644c7f0c1ebaf66a6e
vmose <vicmose.vm@gmail.com>
2024-09-29 19:08:34 +0300

    final update 20240929 1908h

--- superside_hr/analyses/staff_by_department.sql
+++ superside_hr/analyses/staff_by_department.sql
@@ -1,4 +1,5 @@
 -- analyses/staff_by_department.sql
+-- here I included a sample ad hoc query as an example though not part of the model
 SELECT business_group, COUNT(DISTINCT(staff_id)) AS staff_count
 FROM {{ ref('final_staff_model') }}
 GROUP BY business_group;
--- superside_hr/macros/date_helpers.sql
+++ superside_hr/macros/date_helpers.sql
@@ -1,4 +1,5 @@
 -- macros/date_helpers.sql
+-- examplr of reusable date handling/transformations, i.e., date parsing
 {% macro parse_date(date_str) %}
   PARSE_DATE('%Y-%m-%d', {{ date_str }})
 {% endmacro %}
