faa4d8329bd3320105bb2d901de9539f7231d657
vmose <80821950+vmose@users.noreply.github.com>
2024-10-15 16:58:20 +0300

    updated 20241015 1700h

--- resource_manager_model/macros/date_helpers.sql
+++ resource_manager_model/macros/date_helpers.sql
@@ -1,5 +1,5 @@
 -- macros/date_helpers.sql
--- examplr of reusable date handling/transformations, i.e., date parsing
+-- example of reusable date handling/transformations, i.e., date parsing
 {% macro parse_date(date_str) %}
   PARSE_DATE('%Y-%m-%d', {{ date_str }})
 {% endmacro %}
